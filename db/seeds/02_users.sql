85eedbaa46a719b592e0f2e16b62e248d96ab29b
Yves Candau <ycandau@gmail.com>
2021-07-24 01:09:09 +0000

    Add SQL seeds for users

---
+++ db/seeds/02_users.sql
@@ -0,0 +1,7 @@
+INSERT INTO users (id, login, password, name, date_created)
+VALUES (1, 'rfripp', 'password', 'Robert Fripp', '1972-08-23'),
+(2, 'pgabriel', 'password', 'Peter Gabriel', '1975-04-17'),
+(3, 'sreich', 'password', 'Steve Reich', '1969-06-19'),
+(4, 'jadams', 'password', 'John Adams', '1979-02-04'),
+(5, 'tyorke', 'password', 'Thom Yorke', '1985-11-09'),
+(6, 'pselway', 'password', 'Philip Selway', '1987-07-12');
