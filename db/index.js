2ccd6d692460a781f8b069b6df6bcbea97fe850a
Yves Candau <ycandau@gmail.com>
2021-07-28 18:49:10 +0000

    Clean up redundant files

--- db/index.js
+++ db/index.js
@@ -17,18 +17,3 @@ const { Pool } = require('pg');
 const db = new Pool(dbParams);
 
 module.exports = db;
-
-/**-----------------------------------------------------------------------------
- * Get all passwords to which a user has access
- * @param {String} user_id The id of the user.
- * @param {String} limit The maximum number of passwords to get.
- * @return {Promise<{}>} A promise to the password list.
- */
-
-// const getAllPasswords = (user_id, limit = 10) =>
-//   db
-//     .query(`SELECT * FROM passwords;`)
-//     .then((res) => res.rows || null)
-//     .catch((err) => console.error('query error', err.stack));
-
-// exports.getAllPasswords = getAllPasswords;
